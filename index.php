7b51fdf30a6057977c0d4683458b9a3ed83c271f
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-18 16:55:56 +0200

    [UPD] iterate through hotels and attributes using foreach and printing data on the page

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +52,19 @@
 </head>
 
 <body>
-
+    <h1>Lista degli Hotels</h1>
+    <p>
+        <?php
+        // inizio del ciclo principale per scorrere tutti gli hotel 
+        foreach ($hotels as $hotel) {
+            // inizia del ciclo interno per scorrere ogn attributo di un singolo hotel 
+            foreach ($hotel as $key => $value) {
+                // stampa la chiave e il valore dell'attributo corrente dell'hotel
+                echo $key . ' : ' . $value . '<br>';
+            }
+        }
+        ?>
+    </p>
 </body>
 
 </html>
